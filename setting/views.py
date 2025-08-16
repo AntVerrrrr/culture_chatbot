58df6b41aaf3b04160d4ddd129094ece07f4e9f4
AntVerrrrr <jhs789654123@gmail.com>
2025-08-16 21:54:29 +0900

    chore : 쓸모없는 라우터 제거

--- setting/views.py
+++ setting/views.py
@@ -5,7 +5,3 @@ from django.utils.translation import get_language
 def setting_home(request):
     return render(request, 'setting/base_setting.html')
 
-def ping_lang(request):
-    # lang = request.headers.get("X-Language", "ko")
-    lang = get_language()
-    return JsonResponse({"lang": lang})
