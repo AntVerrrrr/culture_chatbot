7db7ca01b58a82b6180625a367c6dd9be6ead236
AntVerrrrr <jhs789654123@gmail.com>
2025-08-16 21:54:48 +0900

    chore : 쓸모없는 라우터 제거

--- setting/urls.py
+++ setting/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from . import views
-from .views import ping_lang
 
 urlpatterns = [
-    path('', views.setting_home, name='setting'),  # /setting/으로 접속 시
-    path('ping-lang/', ping_lang, name='ping-lang'),
+    path("", views.setting_home, name="setting"),  # 빈 경로여야 /<lang>/setting/과 매칭됨
 ]
